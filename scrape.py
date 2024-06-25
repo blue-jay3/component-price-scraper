fae6d53678715c99e53e3e36ca46da01d9da818f
blue-jay3 <emferra01@gmail.com>
2024-06-24 23:07:13 -0400

    added home button, pin and unpin items which will update tables in postgresql database, link icon opens product page in new tab

--- scrape.py
+++ scrape.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import psycopg2
-from datetime import datetime
+from datetime import date, datetime
 from settings import *
 
 def connect_to_database():
@@ -11,9 +11,73 @@ def connect_to_database():
 
     return cursor, connection
 
+def updatePinnedPrices(): # schedule to call this each day to update all pinned prices
+    todayDate = date.today().isoformat()
+    cursor, connection = connect_to_database()
+
+    cursor.execute("SELECT DISTINCT product_name FROM pinned_item_price_records;")
+
+    records = cursor.fetchall()
+    for row in records:
+        cursor.execute(f"SELECT url FROM pinned_items WHERE product_name='{row['product_name']}';")
+        url = cursor.fetchone()
+        page_to_scrape = requests.get(url)
+        soup = BeautifulSoup(page_to_scrape.content, 'html5lib')
+
+        cursor.execute(f"SELECT date FROM pinned_item_price_records WHERE product_name='{row['product_name']}';")
+        dates = cursor.fetchall()
+        if todayDate not in dates:
+            price = soup.findAll("span", attrs={"class":"h2-big"})[0].text
+            cursor.execute(f"INSERT INTO pinned_item_price_records VALUES (DATE '{todayDate}', '{row['product_name']}', {price});")
+            connection.commit()
+        else:
+            print("Price already logged on " + str(todayDate))
+    
+    cursor.close()
+    connection.close()
+
+def saveCurrentPrice(name, price):
+    todayDate = date.today()
+    cursor, connection = connect_to_database()
+
+    cursor.execute(f"SELECT date FROM pinned_item_price_records WHERE product_name='{name}';")
+    dates = cursor.fetchall()
+    if len(dates) == 0 or todayDate not in dates[0]:
+        cursor.execute(f"INSERT INTO pinned_item_price_records VALUES (DATE '{todayDate.isoformat()}', '{name}', {price});")
+        connection.commit()
+    else:
+        print("Price already logged on " + str(todayDate.isoformat()))
+
+    cursor.close()
+
+def pin(name, price, url):
+    currentDT = datetime.now()
+    cursor, connection = connect_to_database()
+
+    cursor.execute(f"SELECT COUNT(1) FROM pinned_items WHERE product_name='{name}';")
+    results = cursor.fetchone()
+    if results[0] >= 1:
+        unpin(name)
+        return
+
+    cursor.execute(f"INSERT INTO pinned_items VALUES (TIMESTAMP '{currentDT}', '{name}', '{url}');")
+    connection.commit()
+
+    saveCurrentPrice(name, price)
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+def unpin(name):
+    cursor, connection = connect_to_database()
+
+    cursor.execute(f"DELETE FROM pinned_items WHERE product_name='{name}';")
+    connection.commit()
+
 def update_prices(search, cpath):
     cursor, connection = connect_to_database()
-    cursor.execute("TRUNCATE TABLE price_records;")
+    cursor.execute("TRUNCATE TABLE search_results;")
     connection.commit()
 
     if cpath != "":
@@ -35,33 +99,38 @@ def update_prices(search, cpath):
         price = element.findAll("span", attrs={"class":["d-block", "mb-0", "pq-hdr-product_price"]})
 
         # save the product info to the database
-        cursor.execute(f"""INSERT INTO price_records VALUES (TIMESTAMP '{today}', '{variable.text}', {price[1].text[1:].replace(",", "")});""")
+        cursor.execute(f"""INSERT INTO search_results VALUES (TIMESTAMP '{today}', '{variable.text}', {price[1].text[1:].replace(",", "")}, '{variable.get('href')}');""")
 
     connection.commit()
 
-    cursor.execute("SELECT product_name, price from price_records;")
+    cursor.execute("SELECT product_name, price, url from search_results;")
     records = cursor.fetchall()
     records.sort(key=lambda x: x[1]) # sort by price
 
+    cursor.close()
+    connection.close()
     return records
 
 def get_lowest():
     cursor, _ = connect_to_database()
 
-    cursor.execute("SELECT * FROM price_records WHERE price = (SELECT MIN(price) FROM price_records);")
+    cursor.execute("SELECT * FROM search_results WHERE price = (SELECT MIN(price) FROM search_results);")
     min_price = cursor.fetchone()
 
+    cursor.close()
     return min_price
 
 # display the records currently in the db
 def view_database():
     cursor, _ = connect_to_database()
 
-    cursor.execute("SELECT * from price_records;")
+    cursor.execute("SELECT * from search_results;")
     records = cursor.fetchall()
 
     for row in records:
         print(row)
+    
+    cursor.close()
 
 if __name__ == "__main__":
     cursor, connection = connect_to_database()
--- server.py
+++ server.py
@@ -1,33 +1,45 @@
-from flask import Flask, render_template, request
-from scrape import update_prices
+from flask import Flask, redirect, render_template, request, url_for
+from scrape import update_prices, pin
 from waitress import serve
 
 app = Flask(__name__)
+current_search = ""
+current_category = ""
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
+@app.route('/pin') # ?
+def pin_item():
+    name = request.args.get('product_name')
+    price = request.args.get('product_price')
+    url = request.args.get('product_url')
+    pin(name, price, url)
+    return redirect("/prices")
+
 @app.route('/prices')
 def get_lowest_price():
     item = request.args.get('search')
     cpath = request.args.get('category')
 
-    if item == None or item.strip() == "":
-        item = "rtx 4070"
+    if item == None or item.strip() == "": # update
+        current_search = "rtx 4070"
+        current_category = ""
+    else:
+        current_search = item
+        current_category = cpath if cpath != None else ""
 
-    item_info = update_prices(item, cpath)
+    item_info = update_prices(current_search, current_category)
 
     if item_info != None and item_info != []:
         return render_template(
             'prices.html',
-            title=item,
-            #name=item_info[1].upper(),
-            #price="$" + str(item_info[2])
+            title=current_search,
             records=item_info
         )
-    else:
+    else: # fix
         return render_template(
             'prices.html',
             title=item,
